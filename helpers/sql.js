a92d7ddd8d3dce4cde64c901711856fae78e9fa0
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-29 11:46:35 +0400

    add documentation for the sqlForPartialUpdate function

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,23 @@
 const { BadRequestError } = require('../expressError');
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Generates SQL column assignment string and corresponding values array for a partial update.
+ *
+ * The function takes an object of data to update and a mapping object to convert JavaScript
+ * object keys to SQL column names. It is useful for dynamically creating the SET part
+ * of an SQL UPDATE statement.
+ *
+ * @param {Object} dataToUpdate - An object where keys are object fields to update and values are the new values.
+ * @param {Object} jsToSql - A mapping object from JavaScript camelCase keys to SQL snake_case column names.
+ * @returns {Object} An object with two properties: `setCols` (String), a comma-separated column assignment string,
+ *                   and `values` (Array), an array of values corresponding to the placeholders in `setCols`.
+ *
+ * @throws {BadRequestError} If `dataToUpdate` is empty.
+ *
+ * @example
+ * returns { setCols: "\"first_name\"=$1, \"age\"=$2", values: ["Aliya", 32] }
+ * sqlForPartialUpdate({ firstName: 'Aliya', age: 32 }, { firstName: "first_name" });
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
     const keys = Object.keys(dataToUpdate);
