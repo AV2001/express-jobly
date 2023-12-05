6a73b0454e895e0c2ec31a282513c26b74805ec6
AV2001 <anirudhvadlamani2001@gmail.com>
2023-12-05 22:10:58 +0400

    remove dupilcate check in create method since multiple roles can share same details

--- models/job.js
+++ models/job.js
@@ -5,25 +5,12 @@ const { BadRequestError } = require('../expressError');
 
 class Job {
     static async create({ title, salary, equity, companyHandle }) {
-        const duplicateCheck = await db.query(
-            `
-            SELECT company_handle
-            FROM jobs
-            WHERE company_handle = $1
-            `,
-            [companyHandle]
-        );
-
-        if (duplicateCheck.rows[0]) {
-            throw new BadRequestError(`Duplicate company: ${companyHandle}`);
-        }
-
         const result = await db.query(
             `
             INSERT INTO jobs
             (title, salary, equity, company_handle)
             VALUES ($1, $2, $3, $4)
-            RETURNNG title, salary, equity, company_handle
+            RETURNNG title, salary, equity, company_handle AS "companyHandle"
             `,
             [title, salary, equity, companyHandle]
         );
