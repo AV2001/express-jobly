b6849f3020bc1c818f838f821904e4ae2f89c208
AV2001 <anirudhvadlamani2001@gmail.com>
2023-12-06 23:06:44 +0400

    allow use of jobsRoutes

---
+++ routes/jobs.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Routes for jobs.
+
+const jsonschema = require('jsonschema');
+const express = require('express');
+
+const Job = require('../models/job');
+
+const router = express.Router();
+
+module.exports = router;
