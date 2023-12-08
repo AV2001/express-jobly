314be707167966a7f073227eeaf2bae325ae246d
AV2001 <anirudhvadlamani2001@gmail.com>
2023-12-08 08:51:51 +0400

    add GET /jobs route for getting all jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -9,4 +9,19 @@ const Job = require('../models/job');
 
 const router = express.Router();
 
+/**
+ * GET / =>
+ * {jobs: [{title, salary, equity, companyHandle}, ...]}
+ *
+ * Authorization required: none
+ */
+router.get('/', async (req, res, next) => {
+    try {
+        const jobs = await Job.findAll();
+        return res.json({ jobs });
+    } catch (err) {
+        return next(err);
+    }
+});
+
 module.exports = router;
