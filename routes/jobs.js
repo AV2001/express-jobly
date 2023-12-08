0490ef57ef6e05b598600b0396b2b9157f6567f7
AV2001 <anirudhvadlamani2001@gmail.com>
2023-12-08 12:44:50 +0400

    add POST /jobs to create a new job

--- routes/jobs.js
+++ routes/jobs.js
@@ -5,7 +5,11 @@
 const jsonschema = require('jsonschema');
 const express = require('express');
 
+const { ensureLoggedIn, ensureUserIsAdmin } = require('../middleware/auth');
 const Job = require('../models/job');
+
+const jobNewSchema = require('../schemas/jobNew.json');
+const { BadRequestError } = require('../expressError');
 
 const router = express.Router();
 
@@ -19,6 +23,30 @@ router.get('/', async (req, res, next) => {
     try {
         const jobs = await Job.findAll();
         return res.json({ jobs });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+/**
+ * POST / {job} => {job}
+ *
+ * job should be {title, salary, equity, companyHandle}
+ *
+ * Returns {title, salary, equity, companyHandle}
+ *
+ * Authorization required: login
+ */
+router.post('/', ensureUserIsAdmin, async (req, res, next) => {
+    try {
+        const validator = jsonschema.validate(req.body, jobNewSchema);
+        if (!validator.valid) {
+            const errors = validator.errors.map((e) => e.stack);
+            throw new BadRequestError(errors);
+        }
+
+        const job = await Job.create(req.body);
+        return res.status(201).json({ job });
     } catch (err) {
         return next(err);
     }
