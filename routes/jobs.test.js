84e6cbf7768aead4211c4fd2e443a3f0e216ac08
AV2001 <anirudhvadlamani2001@gmail.com>
2023-12-10 15:50:23 +0400

    add tests for the POST /jobs route

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -13,6 +13,7 @@ const {
     commonAfterEach,
     commonAfterAll,
     testJob,
+    u1Token,
 } = require('./_testCommon');
 const { createToken } = require('../helpers/tokens');
 
@@ -41,5 +42,64 @@ describe('GET /jobs/:id', () => {
     test('Returns 404 if invalid id is passed', async () => {
         const response = await request(app).get('/jobs/0');
         expect(response.statusCode).toBe(404);
+    });
+});
+
+// POST /jobs
+describe('POST /jobs', () => {
+    test('Create a job as an admin', async () => {
+        const newJob = {
+            title: 'Software Engineer',
+            salary: 200000,
+            equity: 0.1,
+            companyHandle: 'c1',
+        };
+        const response = await request(app)
+            .post('/jobs')
+            .send(newJob)
+            .set('authorization', `Bearer ${u1Token}`);
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toEqual({
+            job: {
+                ...newJob,
+                id: expect.any(Number),
+                equity: expect.any(String),
+            },
+        });
+    });
+
+    test('Returns 400 if missing job data', async () => {
+        const response = await request(app)
+            .post('/jobs')
+            .set('authorization', `Bearer ${u1Token}`);
+        expect(response.statusCode).toBe(400);
+    });
+
+    test('Returns 400 if invalid companyHandle is provided', async () => {
+        const newJob = {
+            salary: 200000,
+            equity: 0.1,
+            companyHandle: 'google',
+        };
+        const response = await request(app)
+            .post('/jobs')
+            .send(newJob)
+            .set('authorization', `Bearer ${u1Token}`);
+        expect(response.statusCode).toBe(400);
+    });
+
+    test('Returns 401 if non-admin user creates a job', async () => {
+        const newUserToken = createToken({ username: 'user', isAdmin: false });
+        const newJob = {
+            title: 'Software Engineer',
+            salary: 200000,
+            equity: 0.1,
+            companyHandle: 'c1',
+        };
+        const response = await request(app)
+            .post('/jobs')
+            .send(newJob)
+            .set('authorization', `Bearer ${newUserToken}`);
+        expect(response.statusCode).toBe(401);
     });
 });
