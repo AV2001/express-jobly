3f3acedb48cce0b0f16f01d99f0c14b04ca1b202
AV2001 <anirudhvadlamani2001@gmail.com>
2023-12-10 15:19:20 +0400

    add tests for GET /jobs/:id route

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -29,3 +29,17 @@ describe('GET /jobs', () => {
         expect(response.body).toEqual({ jobs: [testJob()] });
     });
 });
+
+// GET /jobs/:id
+describe('GET /jobs/:id', () => {
+    test('Return a job', async () => {
+        const response = await request(app).get(`/jobs/${testJob().id}`);
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({ job: testJob() });
+    });
+
+    test('Returns 404 if invalid id is passed', async () => {
+        const response = await request(app).get('/jobs/0');
+        expect(response.statusCode).toBe(404);
+    });
+});
