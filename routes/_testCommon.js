1a93040b7a7216c0e588f0b16a03b14bf94d04c5
AV2001 <anirudhvadlamani2001@gmail.com>
2023-12-10 14:42:59 +0400

    create a job for testing

--- routes/_testCommon.js
+++ routes/_testCommon.js
@@ -3,6 +3,7 @@
 const db = require('../db.js');
 const User = require('../models/user');
 const Company = require('../models/company');
+const Job = require('../models/job');
 const { createToken } = require('../helpers/tokens');
 
 async function commonBeforeAll() {
@@ -10,6 +11,8 @@ async function commonBeforeAll() {
     await db.query('DELETE FROM users');
     // noinspection SqlWithoutWhere
     await db.query('DELETE FROM companies');
+
+    await db.query('DELETE FROM jobs');
 
     await Company.create({
         handle: 'c1',
@@ -56,6 +59,12 @@ async function commonBeforeAll() {
         email: 'user3@user.com',
         password: 'password3',
         isAdmin: false,
+    });
+    await Job.create({
+        title: 'Software Engineer',
+        salary: 200000,
+        equity: 0.5,
+        companyHandle: 'c1',
     });
 }
 
