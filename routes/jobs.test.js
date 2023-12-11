324e5241566c390ebe67bafe8cc989a05296818b
AV2001 <anirudhvadlamani2001@gmail.com>
2023-12-11 18:57:02 +0400

    add tests for DELETE /jobs/:id route

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -24,7 +24,7 @@ afterAll(commonAfterAll);
 
 // GET /jobs
 describe('GET /jobs', () => {
-    test('Returns all jobs', async () => {
+    test('returns all jobs', async () => {
         const response = await request(app).get('/jobs');
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({ jobs: [testJob()] });
@@ -33,13 +33,13 @@ describe('GET /jobs', () => {
 
 // GET /jobs/:id
 describe('GET /jobs/:id', () => {
-    test('Return a job', async () => {
+    test('return a job', async () => {
         const response = await request(app).get(`/jobs/${testJob().id}`);
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({ job: testJob() });
     });
 
-    test('Returns 404 if invalid id is passed', async () => {
+    test('returns 404 if invalid id is passed', async () => {
         const response = await request(app).get('/jobs/0');
         expect(response.statusCode).toBe(404);
     });
@@ -47,7 +47,7 @@ describe('GET /jobs/:id', () => {
 
 // POST /jobs
 describe('POST /jobs', () => {
-    test('Create a job as an admin', async () => {
+    test('create a job as an admin', async () => {
         const newJob = {
             title: 'Software Engineer',
             salary: 200000,
@@ -68,14 +68,14 @@ describe('POST /jobs', () => {
         });
     });
 
-    test('Returns 400 if missing job data', async () => {
+    test('returns 400 if missing job data', async () => {
         const response = await request(app)
             .post('/jobs')
             .set('authorization', `Bearer ${u1Token}`);
         expect(response.statusCode).toBe(400);
     });
 
-    test('Returns 400 if invalid companyHandle is provided', async () => {
+    test('returns 400 if invalid companyHandle is provided', async () => {
         const newJob = {
             salary: 200000,
             equity: 0.1,
@@ -88,7 +88,7 @@ describe('POST /jobs', () => {
         expect(response.statusCode).toBe(400);
     });
 
-    test('Returns 401 if non-admin user creates a job', async () => {
+    test('returns 401 if non-admin user creates a job', async () => {
         const newUserToken = createToken({ username: 'user', isAdmin: false });
         const newJob = {
             title: 'Software Engineer',
@@ -106,7 +106,7 @@ describe('POST /jobs', () => {
 
 // PATCH /jobs/:id
 describe('PATCH /jobs/:id', () => {
-    test('Update a job successfully as an admin', async () => {
+    test('update a job successfully as an admin', async () => {
         const updatedJob = { title: 'Senior Software Engineer' };
         const response = await request(app)
             .patch(`/jobs/${testJob().id}`)
@@ -123,7 +123,7 @@ describe('PATCH /jobs/:id', () => {
         });
     });
 
-    test('Returns 400 if id is updated', async () => {
+    test('returns 400 if id is updated', async () => {
         const updatedJob = { id: 100 };
         const response = await request(app)
             .patch(`/jobs/${testJob().id}`)
@@ -132,7 +132,7 @@ describe('PATCH /jobs/:id', () => {
         expect(response.statusCode).toBe(400);
     });
 
-    test('Returns 400 if companyHandle is updated', async () => {
+    test('returns 400 if companyHandle is updated', async () => {
         const updatedJob = {
             companyHandle: 'google',
         };
@@ -143,7 +143,7 @@ describe('PATCH /jobs/:id', () => {
         expect(response.statusCode).toBe(400);
     });
 
-    test('Returns 400 on invalid data', async () => {
+    test('returns 400 on invalid data', async () => {
         const updatedJob = { salary: '200000' };
         const response = await request(app)
             .patch(`/jobs/${testJob().id}`)
@@ -152,14 +152,14 @@ describe('PATCH /jobs/:id', () => {
         expect(response.statusCode).toBe(400);
     });
 
-    test('Returns 400 if job data us empty', async () => {
+    test('returns 400 if job data us empty', async () => {
         const response = await request(app)
             .patch(`/jobs/${testJob().id}`)
             .set('authorization', `Bearer ${u1Token}`);
         expect(response.statusCode).toBe(400);
     });
 
-    test('Returns 401 if non-admin updates a job', async () => {
+    test('returns 401 if non-admin updates a job', async () => {
         const newUserToken = createToken({ username: 'user', isAdmin: false });
         const newJob = {
             title: 'Software Engineer',
@@ -171,6 +171,29 @@ describe('PATCH /jobs/:id', () => {
             .patch(`/jobs/${testJob().id}`)
             .send(newJob)
             .set('authorization', `Bearer ${newUserToken}`);
+        expect(response.statusCode).toBe(401);
+    });
+});
+
+// DELETE /jobs/:id
+describe('DELETE /jobs/:id', () => {
+    test('admin can delete a job', async () => {
+        const response = await request(app)
+            .delete(`/jobs/${testJob().id}`)
+            .set('authorization', `Bearer ${u1Token}`);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('returns 404 if invalid id is provided', async () => {
+        const response = await request(app)
+            .delete('/jobs/1')
+            .set('authorization', `Bearer ${u1Token}`);
+        expect(response.statusCode).toBe(404);
+    });
+
+    test('returns 401 if non-admin user deletes a job', async () => {
+        const newUserToken = createToken({ username: 'user', isAdmin: false });
+        const response = await request(app).delete(`/jobs/${testJob().id}`);
         expect(response.statusCode).toBe(401);
     });
 });
