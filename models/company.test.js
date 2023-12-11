bd2fe9ff27a6757bc7c2880022720ee3ce5dd535
AV2001 <anirudhvadlamani2001@gmail.com>
2023-12-12 02:02:32 +0400

    modify test to also get an array of jobs associated with the company

--- models/company.test.js
+++ models/company.test.js
@@ -8,6 +8,7 @@ const {
     commonBeforeEach,
     commonAfterEach,
     commonAfterAll,
+    testJob,
 } = require('./_testCommon');
 
 beforeAll(commonBeforeAll);
@@ -99,6 +100,14 @@ describe('get', function () {
             description: 'Desc1',
             numEmployees: 1,
             logoUrl: 'http://c1.img',
+            jobs: [
+                {
+                    id: expect.any(Number),
+                    title: 'Software Engineer',
+                    salary: 200000,
+                    equity: '0.5',
+                },
+            ],
         });
     });
 
