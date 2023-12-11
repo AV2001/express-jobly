b69ac7da15a73359c3ba29caec761444d403b113
AV2001 <anirudhvadlamani2001@gmail.com>
2023-12-11 19:18:17 +0400

    add tests for findAll method

--- models/job.test.js
+++ models/job.test.js
@@ -15,8 +15,8 @@ beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
 
-// Create
-describe('CREATE', () => {
+// create
+describe('create', () => {
     const newJob = {
         title: 'Software Test Engineer',
         salary: 160000,
@@ -27,5 +27,21 @@ describe('CREATE', () => {
     test('works', async () => {
         const job = await Job.create(newJob);
         expect(job).toEqual({ ...newJob, equity: '0', id: expect.any(Number) });
+    });
+});
+
+// findAll
+describe('findAll', () => {
+    test('works: no filter', async () => {
+        const jobs = await Job.findAll();
+        expect(jobs).toEqual([
+            {
+                id: expect.any(Number),
+                title: 'Software Engineer',
+                salary: 200000,
+                equity: '0.5',
+                companyHandle: 'c1',
+            },
+        ]);
     });
 });
